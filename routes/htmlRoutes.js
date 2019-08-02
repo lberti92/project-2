3291f115b15e6765df4d88414f5f7a17ae5833b7
Mads <madisonmartinez@Alicias-MacBook-Pro.local>
2019-08-01 20:35:34 -0600

    starting templates

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,3 @@
 // Middleware to ensure that a user is logged-in in order to complete a request
-var isAuthenticated = require("../config/isAuthenticated");
+var isAuthenticated = require("../config/auth.js");
 
-var express = require("express");
-var router = express.Router();
-
-//create routes and logic
-router.get("/", function (req, res){
-        res.render("index");
-    });
