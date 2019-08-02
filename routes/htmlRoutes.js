e4c9870ba8fd9a9d5bb7dbb0e9d562ec059bf71f
Mads <madisonmartinez@Alicias-MacBook-Pro.local>
2019-08-01 20:12:16 -0600

    experiment

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,2 +1,10 @@
 // Middleware to ensure that a user is logged-in in order to complete a request
 var isAuthenticated = require("../config/isAuthenticated");
+
+var express = require("express");
+var router = express.Router();
+
+//create routes and logic
+router.get("/", function (req, res){
+        res.render("index");
+    });
