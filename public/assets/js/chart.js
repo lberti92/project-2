1693617252081e3621391521aa32103079d5bc0a
Mads <madisonmartinez@Alicias-MacBook-Pro.local>
2019-08-03 21:44:17 -0600

    added type, flavor, rating and underage pages along with some routes and some functions to catch data

--- public/assets/js/chart.js
+++ public/assets/js/chart.js
@@ -1,1 +1,20 @@
-
+new Chart(document.getElementById("pie-chart"), {
+    type: 'pie',
+    data: {
+      labels: ["Woody/Nutty", "Caramel", "Herbal Spice", "Citrus", "Floral", "Fruity", "Vanilla", "None/Mild"],
+      datasets: [{
+        label: "List of Flavors",
+        backgroundColor: ["#6E2C00", "#F8C471","#C0392B","#FFA500","#BB8FCE", "#FF1493", "#FEF9E7", "#85929E"],
+        data: [450,450,450,450,450,450,450,450]
+      }]
+    },
+    options: {
+        legend: {
+            display: false,
+        }, 
+        //this tells us which array index is clicked on ex: Herbal Spice is 2
+        "onClick" : function (event, item){
+            console.log(item[0]._index);
+        }
+    }
+ });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,16 +4,36 @@ var path = require("path");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
-  app.get("/users/register", function(req, res) {
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    res.render("index")
+  });
+
+  app.get("/users/register", function (req, res) {
     res.render("register");
   });
 
-  app.get("/users/login", function(req, res) {
-      res.render("login");
+  app.get("/users/login", function (req, res) {
+    res.render("login");
   });
-  
-  app.get("/", function (req,res){
-      res.render("flavor");
+
+  app.get("/homepage", function (req, res) {
+    res.render("homepage")
+  });     
+
+  app.get("/types", function (req, res) {
+    res.render("type")
+  });
+
+  app.get("/flavor", function (req, res){
+    res.render("flavor")
+  });
+
+  app.get("/ratings", function (req, res){
+    res.render("rating")
+  });
+
+  app.get("/underage", function (req, res){
+    res.sendFile(path.join(__dirname + "/underage.html"));
   })
 };
